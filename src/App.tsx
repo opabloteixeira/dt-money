289cff9907e7c7246b3e4954a6d996af6d9d4cca
Pablo <pablopst@gmail.com>
2021-06-11 18:53:20 -0300

    finished modal

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal';
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 Modal.setAppElement('#root');
 
@@ -18,12 +19,11 @@ export function App() {
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal
+
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h1>ola modal</h1>
-      </Modal>
+      />
       <GlobalStyle/>
     </>
   );
---
+++ src/components/NewTransactionModal/index.tsx
@@ -0,0 +1,40 @@
+
+import Modal from 'react-modal';
+import { Form } from './styles';
+
+interface NewTransactionModalProps {
+  isOpen: boolean;
+  onRequestClose: () => void;
+}
+
+
+export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  return (
+    <Modal
+      isOpen={isOpen}
+      onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className='react-modal-content'
+
+    >
+      <Form>
+        <h2>Cadastrar transação</h2>
+
+        <input
+          placeholder="Título"
+        />
+
+        <input
+          type="number"
+          placeholder="Valor"
+        />
+
+        <input placeholder="Categoria" />
+
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Form>
+    </Modal>
+  )
+}
